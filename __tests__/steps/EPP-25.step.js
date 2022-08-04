b128081bb8a62febbfa5aa9ea18f74cabe3c2806
Putra Aditya Wicaksono <putra.aditya@photoninfotech.net>
2022-08-04 18:24:00 +0700

    Add unit test for EPP-25

---
+++ __tests__/steps/EPP-25.step.js
@@ -0,0 +1,61 @@
+import { fireEvent, render } from "@testing-library/react";
+import { defineFeature, loadFeature } from "jest-cucumber";
+import ForgotPassword from "../../src/components/organisms/ForgotPassword/forgotPassword";
+
+const feature = loadFeature(
+  "./__tests__/features/Patient Portal/Sprint2/EPP-25.feature"
+);
+
+defineFeature(feature, (test) => {
+  test("EPIC_EPP-7 _STORY_EPP-25 - Verify user should see inline error below Email or Phone Number field if Email or Phone Number is blank", ({
+    given,
+    when,
+    then,
+    and,
+  }) => {
+    let container;
+    given("user launch the 'XXX' url", () => {
+      container = render(<ForgotPassword onBackToLoginClicked={()=>{}}/>);
+    });
+
+    and("user navigates to the Patient Portal application", () => {
+        expect(true).toBeTruthy()
+    });
+
+    when(`user lands onto "Patient Login" screen`, () => {
+        expect(true).toBeTruthy()
+    });
+
+    then(
+      "user should see 'Forgot Password' link",
+      () => {
+        expect(true).toBeTruthy()
+      }
+    );
+
+    when(`user clicks on 'Forgot Password' link`, () => {
+        expect(true).toBeTruthy()
+    });
+
+    then(`user should see Forgot Password screen`, () => {
+        const title = container.getByText("title");
+        expect("title").toEqual(title.textContent);
+    });
+
+    and("user should see 'Email or Phone Number' field", () => {
+        const usernameField = container.getByLabelText(/usernamePlaceHolder/i);
+        fireEvent.change(usernameField, { target: { value: "username@4A" } });
+        expect(usernameField.value).toEqual("username@4A");
+    });
+
+    and("user should see 'Continue' button", () => {
+        const continueId = container.getByRole("button", { name: /resetPasswordText/i });
+        fireEvent.click(continueId);
+    });
+
+    and("user should see 'Back to Login' link", () => {
+        const link = container.getByText("backButtonLink");
+        fireEvent.click(link);
+    });
+  });
+});
