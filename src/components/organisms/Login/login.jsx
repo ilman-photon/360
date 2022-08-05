2fc73bcc4d92a66025c54c59da8959c8f82da4d8
Pratama Indra Raswoko <pratamaindra.r@photoninfotech.net>
2022-08-05 20:42:45 +0700

    EPP-2293: Patient Portal | Login Screen | Incorrect error message is displayed if user clicks on login with empty username and password

--- src/components/organisms/Login/login.jsx
+++ src/components/organisms/Login/login.jsx
@@ -91,7 +91,7 @@ export default function Login({
                 );
               }}
               rules={{
-                required: t("emailRequiredLabel"),
+                required: t("thisFieldRequired"),
               }}
             />
             <Controller
