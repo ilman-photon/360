8c6633a302c8d3581496e75de909ff362161b55d
Ilman Teguh Prasetya <ilman.teguh@photon.com>
2022-08-08 11:17:11 +0700

    fix 210

--- __tests__/steps/EPIC_EPP-4_STORY_EPP-210-Verify.step.js
+++ __tests__/steps/EPIC_EPP-4_STORY_EPP-210-Verify.step.js
@@ -14,7 +14,85 @@ defineFeature(feature, (test) => {
         then,
         and,
       }) => {
-        
+        given(/^user user launch the "(.*)" url$/, (arg0) => {
+
+        });
+
+        and('user user navigates to the Patient Portal application', () => {
+
+        });
+
+        when('user user lands onto “Patient Login” screen', () => {
+
+        });
+
+        and(/^user user provides (.*) and (.*) for "(.*)"$/, (arg0, arg1, arg2) => {
+
+        });
+
+        and(/^user clicks on (.*) button$/, (arg0) => {
+
+        });
+
+        then(/^user should  see the message "(.*)"$/, (arg0) => {
+
+        });
+
+        when(/^user provides (.*) and Invalid (.*) for "(.*)"$/, (arg0, arg1, arg2) => {
+
+        });
+
+        and(/^user clicks on (.*) button$/, (arg0) => {
+
+        });
+
+        then(/^user should see the message "(.*)"$/, (arg0) => {
+
+        });
+
+        when(/^user provides (.*) and Invalid (.*) for "(.*)"$/, (arg0, arg1, arg2) => {
+
+        });
+
+        and(/^user clicks on (.*) button$/, (arg0) => {
+
+        });
+
+        then(/^user should  see the message "(.*)"$/, (arg0) => {
+
+        });
+
+        when(/^user provides (.*) and Invalid (.*) for "(.*)"$/, (arg0, arg1, arg2) => {
+
+        });
+
+        and(/^user clicks on (.*) button$/, (arg0) => {
+
+        });
+
+        then(/^user should  see the message "(.*)"$/, (arg0) => {
+
+        });
+
+        when(/^user provides (.*) and Invalid (.*) for "(.*)"$/, (arg0, arg1, arg2) => {
+
+        });
+
+        and(/^user clicks on (.*) button$/, (arg0) => {
+
+        });
+
+        then('user account should get locked', () => {
+
+        });
+
+        and('user should see the message “Your account has been locked after too many failed attempts. Please contact Customer Support to unlock your account”', () => {
+
+        });
+
+        and('user should receive the mail regarding account lock with below email content.', (table) => {
+
+        });
       });
       test("EPIC_EPP-4_STORY_EPP-210 - Verify if user able to login when enter invalid password for 4 times & valid password attempt for 5th time", ({
         given,
