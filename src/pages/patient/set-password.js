e489af7e56427dbc12b69d22db7e2ad0bb6f4a90
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-08-11 19:48:17 +0700

    fix(set-password): fetch username from url query

--- src/pages/patient/set-password.js
+++ src/pages/patient/set-password.js
@@ -3,16 +3,21 @@ import { useDispatch, useSelector } from "react-redux";
 import { resetFormMessage, setFormMessage } from "../../store";
 import Link from "next/link";
 import { Api } from "../api/api";
-import { useRouter } from "next/router";
 import RESPONSE_MESSAGES from "../../utils/responseCodes";
 import { Box } from "@mui/material";
 import SetPasswordComponent from "../../components/organisms/SetPassword/setPassword";
 import globalStyles from "../../styles/Global.module.scss";
 
-export default function SetPasswordPage() {
+export async function getServerSideProps({ query }) {
+  return {
+    props: {
+      username: query ? query.username : "",
+    },
+  };
+}
+
+export default function SetPasswordPage({ username }) {
   const dispatch = useDispatch();
-  const router = useRouter();
-  const username = router.query.username || "";
 
   const formMessage = useSelector((state) => state.index.formMessage);
 
