7fbcb8078b1c863823ca5f1567a1b2775bb37249
Ilman Teguh Prasetya <ilman.teguh@photon.com>
2022-08-15 17:33:13 +0700

    epp-281-unit test

--- __tests__/steps/EPIC_EPP-4_STORY_EPP-281-Verify.step.js
+++ __tests__/steps/EPIC_EPP-4_STORY_EPP-281-Verify.step.js
@@ -92,6 +92,46 @@ defineFeature(feature, (test) => {
 
   });
 
+  test('EPIC_EPP-3_STORY_EPP-281-Negative-Existing-Verify user should user should see the error message when the internet service is unavailable when user logs in from device using registered mail-id', ({  }) => {
+    test('"EPIC_EPP-3_STORY_EPP-281-Existing-Verify user should be able to login from device that was set up with "Remember me" option selected, without being asked for MFA using registered mail-id"', ({ given, and, when, then }) => {
+      given(/^user launch the "(.*)" url$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+
+      and('user navigates to the Patient Portal application', () => {
+        expect(true).toBeTruthy()
+      });
+
+      when(/^user lands onto "(.*)" screen$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+
+      then(/^user see (.*) and (.*) fields that was MFA was set up$/, (arg0, arg1) => {
+        expect(true).toBeTruthy()
+      });
+
+      and(/^user should fill valid (.*) field with the email$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+
+      and(/^user should fill valid (.*) field$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+
+      and(/^user should see the "(.*)" option has been selected that Remember me has not expired$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+
+      when(/^user clicks on "(.*)" button$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+
+      then(/^user should see "(.*)" screen$/, (arg0) => {
+        expect(true).toBeTruthy()
+      });
+    });
+  });
+
 
 
 
