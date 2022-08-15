51c182ffef8ca3dd10d83554714051c9942c31e5
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-08-15 17:36:29 +0700

    fix(auth-layout): add dependencies when imageBackgrround or showMobileBackground should changed

--- src/components/templates/authLayout.jsx
+++ src/components/templates/authLayout.jsx
@@ -33,7 +33,7 @@ export default function Layout({
       setImageSrcState(imageSrc);
     }
     // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
+  }, [title, imageSrc]);
   return (
     <>
       <Head>
