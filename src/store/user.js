b77d87493830b5a90d3ebce9e0374aa931123009
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-08-18 19:24:44 +0700

    fix: removed code user.js

--- src/store/user.js
+++ src/store/user.js
@@ -4,24 +4,63 @@ export const fetchUser = createAsyncThunk("user/fetchUser", async () => {
   return fetch("/api/dummy/user").then((res) => res.json());
 });
 
+const DEFAULT_USER_DATA = {
+  firstName: "",
+  lastName: "laste",
+  name: "Eyecare User",
+  preferredName: "---",
+  profilePhoto: "",
+  issuedCardFront: "",
+  issuedCardBack: "",
+  dob: new Date(),
+  title: "Mr",
+  ssn: 1234567,
+  email: "",
+  mobile: "",
+  address: "",
+  city: "",
+  state: "",
+  zip: "",
+  preferredCommunication: "both",
+  age: "49",
+  gender: "Male",
+  relationship: "",
+  insurancePriority: "",
+  planName: "",
+  subscriberMember: "",
+  groupId: "",
+  isSubscriber: "",
+};
+
 const userSlice = createSlice({
   name: "user",
   initialState: {
-    userData: {},
+    userData: DEFAULT_USER_DATA,
     status: null,
   },
+  reducers: {
+    resetUserData: (state, action) => {
+      state.loading = action.payload;
+    },
+    setUserData: (state, { payload }) => {
+      state.userData = payload;
+    },
+  },
   extraReducers: {
-    [fetchUser.pending]: (state) => {
+    [fetchUser.pending]: (state, action) => {
       state.status = "loading";
     },
     [fetchUser.fulfilled]: (state, { payload }) => {
       state.userData = payload;
       state.status = "success";
     },
-    [fetchUser.rejected]: (state) => {
+    [fetchUser.rejected]: (state, action) => {
       state.status = "failed";
     },
   },
 });
+
+// Action creators are generated for each case reducer function
+export const { setUserData } = userSlice.actions;
 
 export default userSlice.reducer;
