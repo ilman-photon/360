56e4309a0c423f3bb80fd1575ca54d4d46d6895b
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-08-21 02:14:04 +0700

    set mock date to prevent error on unit test

--- __tests__/src/pages/profileInfo.test.jsx
+++ __tests__/src/pages/profileInfo.test.jsx
@@ -1,14 +1,47 @@
 import { render, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ProfileInformationPage from "../../../src/pages/patient/account/profile-info";
-
 import { Provider } from "react-redux";
 import store from "../../../src/store/store";
 
 describe("ProfileInformationPage Components", () => {
   let container;
+  afterEach(() => {
+    fetch.mockClear();
+  });
   beforeEach(async () => {
-    jest.useFakeTimers().setSystemTime(new Date("2022-08-20"));
+    global.fetch = jest.fn(() =>
+      Promise.resolve({
+        json: () =>
+          Promise.resolve({
+            firstName: "",
+            lastName: "laste",
+            name: "Eyecare User",
+            preferredName: "---",
+            profilePhoto: "",
+            issuedCardFront: "/transparent.png",
+            issuedCardBack: "/transparent.png",
+            dob: new Date(1991, 20, 3),
+            title: "Mr",
+            ssn: 1234567,
+            email: "",
+            mobile: "",
+            address: "",
+            city: "",
+            state: "",
+            zip: "",
+            preferredCommunication: "both",
+            age: "49",
+            gender: "Male",
+            relationship: "",
+            insurancePriority: "",
+            planName: "",
+            subscriberMember: "",
+            groupId: "",
+            isSubscriber: "",
+          }),
+      })
+    );
     container = render(
       <Provider store={store}>
         <ProfileInformationPage />
