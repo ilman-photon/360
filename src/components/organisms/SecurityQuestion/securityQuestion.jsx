34fe48db3d91ae72bce53f00b83006187eb60a37
Ilman Teguh Prasetya <ilman.teguh@photon.com>
2022-08-23 16:42:42 +0700

    EPP-3758

--- src/components/organisms/SecurityQuestion/securityQuestion.jsx
+++ src/components/organisms/SecurityQuestion/securityQuestion.jsx
@@ -12,6 +12,7 @@ const SecurityQuestion = ({
   securityQuestionList = [],
   propsShowPostMessage = false,
   securityQuestionCount = 5,
+
   onClickedSubmitButton = () => {
     // This is intentional
   },
@@ -19,13 +20,42 @@ const SecurityQuestion = ({
     // This is intentional
   },
 }) => {
+  const handleChangeInput = (event) => {
+    const { value, maxLength } = event.target;
+    const message = value.slice(0, maxLength);
+
+    this.setState({
+      form: {
+        message,
+      },
+    });
+  };
   const [showPostMessage, setShowPostMessage] = useState(propsShowPostMessage);
   const [postMessage, setPostMessage] = React.useState(
     "You must answer all security questions"
   );
   const { handleSubmit, control } = useForm();
-
+  const hasDuplicates = (array) => {
+    var valuesSoFar = Object.create(null);
+    for (var i = 0; i < array.length; ++i) {
+      var value = array[i];
+      if (value in valuesSoFar) {
+        return true;
+      }
+      valuesSoFar[value] = true;
+    }
+    return false;
+  };
   const onSubmit = (data) => {
+    const listQuestion = [];
+    for (const [key, value] of Object.entries(data)) {
+      if (key.indexOf("question") > -1) {
+        listQuestion.push(value);
+      }
+    }
+
+    console.log(hasDuplicates(listQuestion));
+
     let validate = true;
     for (const property in data) {
       if (!data[property]) {
@@ -33,7 +63,9 @@ const SecurityQuestion = ({
         break;
       }
     }
-    if (validate) {
+    if (validate && hasDuplicates(listQuestion)) {
+      onShowPostMessage("Don’t choose the same question!");
+    } else if (validate && !hasDuplicates(listQuestion)) {
       const questionAnswer = {};
       const question = [];
       const answer = [];
@@ -46,7 +78,6 @@ const SecurityQuestion = ({
       }
       questionAnswer["question"] = question;
       questionAnswer["answer"] = answer;
-
       onClickedSubmitButton(questionAnswer, checkSubmitMessage);
     } else {
       onShowPostMessage("You must answer all security questions");
@@ -101,6 +132,7 @@ const SecurityQuestion = ({
                   options={securityQuestionList}
                   value={value}
                   onChange={(event) => {
+                    console.log(securityQuestionList);
                     onChange(event);
                     if (showPostMessage) {
                       setShowPostMessage(false);
