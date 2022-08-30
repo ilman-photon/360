9eb3ecc7f35f7e83af669047a6d59eea4e576e62
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-08-30 20:05:53 +0700

    chore: merge from develop

--- src/components/molecules/AutoCompleteCreatable.jsx
+++ src/components/molecules/AutoCompleteCreatable.jsx
@@ -76,12 +76,12 @@ export const AutoCompleteCreatable = ({
           setValue(newValue);
         }
       }}
-      filterOptions={(options, params) => {
+      filterOptions={(optionsContext, params) => {
         const filtered = filter(options, params);
 
         const { inputValue } = params;
         // Suggest the creation of a new value
-        const isExisting = options.some(
+        const isExisting = optionsContext.some(
           (option) => inputValue === option.label
         );
         if (inputValue !== "" && !isExisting) {
