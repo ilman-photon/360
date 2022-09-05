6ab281f01db59a0f7d775c4b0e3ba7553fa27caa
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-09-05 11:16:35 +0700

    fix(EPP-3354): cssfixing sidebar & title heading component

--- src/components/atoms/SidebarLink/sidebarLink.jsx
+++ src/components/atoms/SidebarLink/sidebarLink.jsx
@@ -1,3 +1,4 @@
+import { Typography } from "@mui/material";
 import React from "react";
 import { colors } from "../../../styles/theme";
 import styles from "./sidebarLink.module.scss";
@@ -24,7 +25,14 @@ const SidebarLink = ({ router, href, isLeftSideBar = false, children }) => {
         borderColor: colors.darkGreen,
       }}
     >
-      {children}
+      <Typography
+        variant="allVariants"
+        sx={{
+          color: isCurrentPath ? colors.darkGreen : null,
+        }}
+      >
+        {children}
+      </Typography>
     </a>
   );
 };
