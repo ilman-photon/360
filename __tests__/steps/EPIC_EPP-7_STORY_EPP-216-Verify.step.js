bea6efdd4c17bd61308ddca66a5c05fadb76de5b
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-09-19 12:53:20 +0700

    Update bdd file

--- __tests__/steps/EPIC_EPP-7_STORY_EPP-216-Verify.step.js
+++ __tests__/steps/EPIC_EPP-7_STORY_EPP-216-Verify.step.js
@@ -7,11 +7,14 @@ import { Controller } from "react-hook-form";
 import constants from "../../src/utils/constants";
 
 const feature = loadFeature(
-  "./__tests__/feature/Patient Portal/Sprint2/EPP-216.feature"
+  "./__tests__/feature/Patient Portal/Sprint2/EPP-216.feature",
+  {
+    tagFilter: "@included and not @excluded",
+  }
 );
 
 defineFeature(feature, (test) => {
-  test("EPIC_EPP-7_STORY_EPP-216 - Verify user should be able to login into the patient portal without username & password using the magic link received to my registered mail id.", ({
+  /*test("EPIC_EPP-7_STORY_EPP-216 - Verify user should be able to login into the patient portal without username & password using the magic link received to my registered mail id.", ({
     given,
     when,
     then,
@@ -26,7 +29,7 @@ defineFeature(feature, (test) => {
       expect(true).toBeTruthy();
     });
 
-    when(`user lands onto "Patient Login" screen`, () => {
+    when("user lands onto “Patient Login” screen", () => {
       expect(true).toBeTruthy();
     });
 
@@ -185,5 +188,75 @@ defineFeature(feature, (test) => {
       const link = container.getByText("backButtonLink");
       fireEvent.click(link);
     });
+  });*/
+  test("EPIC_EPP-7_STORY_EPP-216 - Verify user should be able to login into the patient portal without username & password using the magic link received to my registered mail id.", ({
+    given,
+    and,
+    when,
+    then,
+  }) => {
+    given("use launch the 'XXX' url", () => {});
+
+    and("user navigates to the Patient Portal application", () => {});
+
+    when("user lands onto “Patient Login” screen", () => {});
+
+    and("user clicks on 'Forgot Password' link", () => {});
+
+    and(/^user should enter valid (.*)$/, (arg0) => {});
+
+    and(/^user clicks on "(.*)" button$/, (arg0) => {});
+
+    then(/^user should see "(.*)" screen$/, (arg0) => {});
+
+    and(
+      /^user should see "(.*)" button \(if security questions is set or not\)$/,
+      (arg0) => {}
+    );
+
+    and(/^user should see "(.*)" button$/, (arg0) => {});
+
+    and(/^user should see "(.*)" button$/, (arg0) => {});
+
+    when(/^user click on "(.*)" button$/, (arg0) => {});
+
+    then("user should see One-time link page", () => {});
+
+    and(/^user should see "(.*)" text$/, (arg0) => {});
+
+    and(
+      /^user should see "(.*)" options with radio buttons "(.*)" and "(.*)" \(if both are configured during registration\)$/,
+      (arg0, arg1, arg2) => {}
+    );
+
+    and(
+      /^user should select only (\d+) "(.*)" as "(.*)"$/,
+      (arg0, arg1, arg2) => {}
+    );
+
+    when(/^user clicks on "(.*)" button$/, (arg0) => {});
+
+    then(/^user should see heading "(.*)"$/, (arg0) => {});
+
+    and(/^user should see text "(.*)"$/, (arg0) => {});
+
+    and(/^user should see message "(.*)"$/, (arg0) => {});
+
+    when(/^user access the inbox of registered "(.*)"$/, (arg0) => {});
+
+    and("user should receive a magic link mail", () => {});
+
+    and(
+      /^user should see the mail with Email Subject as "(.*)"$/,
+      (arg0) => {}
+    );
+
+    and(/^user should see mail message body as "(.*)"$/, (arg0) => {});
+
+    when("user click on a magic link", () => {});
+
+    then("user should successfully be logged in", () => {});
+
+    and("user should see Home/Dashboard page", () => {});
   });
 });
