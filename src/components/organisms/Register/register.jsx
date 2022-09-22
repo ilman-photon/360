7f0320b7513da5ce78b0885567c9560068a0eda8
Ibnu Siena <ibnu.siena@photon.in>
2022-09-22 13:56:58 +0700

    bug fix EPP-3748

--- src/components/organisms/Register/register.jsx
+++ src/components/organisms/Register/register.jsx
@@ -60,7 +60,7 @@ export default function Register({ OnRegisterClicked, formMessage = null }) {
   const watchedPassword = watch("password", "");
   const [watchedEmail, watchedMobile, watchedPreferredCommunication] = watch([
     "email",
-    "mobile",
+    "mobileNumber",
     "preferredCommunication",
   ]); // you can also target specific fields by their names
   const getRegisteredUsername = () => {
@@ -317,7 +317,7 @@ export default function Register({ OnRegisterClicked, formMessage = null }) {
               return (
                 <StyledInput
                   type="phone"
-                  id="mobile"
+                  id="mobileNumber"
                   data-testid={REGISTER_TEST_ID.mobilenumber}
                   label="Mobile Number"
                   value={value}
