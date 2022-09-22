c43aac8fe6b018ed9aee2f342d046101f2caef4a
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-09-22 11:04:10 +0700

    EPP-5676: implement mock scenario

--- src/pages/api/dummy/appointment/my-appointment/getAllAppointment.js
+++ src/pages/api/dummy/appointment/my-appointment/getAllAppointment.js
@@ -4,10 +4,89 @@ export default function getAllAppointment(req, res) {
   const cookie = new Cookies(req.headers.cookie);
   if (req.method === "GET") {
     let responseData = {
-      appointmentList: [],
+      appointmentList: [
+        {
+          appointmentId: "1",
+          providerInfo: {
+            providerId: "1",
+            name: "Paul Wagner Md",
+            position: "Scripps Eyecare",
+            address: {
+              addressLine1: "51 West 51st Street",
+              addressLine2: "Floor 3, Suite 320 Midtown",
+              city: "Florida",
+              state: "FR",
+              zipcode: "54231",
+            },
+            rating: "5",
+            phoneNumber: "8572999989",
+            distance: "10 mi",
+            image: "/doctor.png",
+            from: "2022-07-18",
+            to: "2022-07-23",
+            location: {
+              latitude: 32.751204,
+              longitude: -117.1641166,
+            },
+          },
+          patientInfo: {
+            name: "Rebecca Chan",
+            firstname: "Rebecca",
+            lastname: "Chan",
+            dob: "12/12/2022",
+            phoneNumber: "1234567890",
+          },
+          appointmentInfo: {
+            appointmentType: "Eye Exam",
+            date: "Wed, 20 Nov 2022 07:30:00 EST",
+            insuranceCarrier: ["ECP Vision", "BlueCare Vision"],
+          },
+        },
+        {
+          appointmentId: "2",
+          providerInfo: {
+            providerId: "1",
+            name: "Dr. Sonha Nguyen",
+            position: "Scripps Eyecare",
+            address: {
+              addressLine1: "51 West 51st Street",
+              addressLine2: "Floor 3, Suite 320 Midtown",
+              city: "Florida",
+              state: "FR",
+              zipcode: "54231",
+            },
+            rating: "5",
+            phoneNumber: "8572999989",
+            distance: "10 mi",
+            image: "/doctor.png",
+            from: "2022-07-18",
+            to: "2022-07-23",
+            location: {
+              latitude: 32.751204,
+              longitude: -117.1641166,
+            },
+          },
+          patientInfo: {
+            name: "Rebecca Chan",
+            firstname: "Rebecca",
+            lastname: "Chan",
+            dob: "12/12/2022",
+            phoneNumber: "1234567890",
+          },
+          appointmentInfo: {
+            appointmentType: "Comprehensive",
+            date: "Wed, 20 Sep 2022 07:30:00 EST",
+            insuranceCarrier: ["ECP Vision", "BlueCare Vision"],
+          },
+        },
+      ],
     };
     const patientId = cookie.get("patientId");
     if (patientId == "7300529f-ce7e-4f8e-946b-92d498a4b03d") {
+      responseData = {
+        appointmentList: [],
+      };
+    } else if (patientId == "ccc5a017-1339-404a-87cc-a8f863b27bdc") {
       responseData = {
         appointmentList: [
           {
@@ -43,7 +122,7 @@ export default function getAllAppointment(req, res) {
             },
             appointmentInfo: {
               appointmentType: "Eye Exam",
-              date: "Wed, 22 Sep 2022 07:30:00 EST",
+              date: "Wed, 23 Sep 2022 07:30:00 EST",
               insuranceCarrier: ["ECP Vision", "BlueCare Vision"],
             },
           },
