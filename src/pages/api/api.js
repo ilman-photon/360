3a6ffe3a8e3d89e565d3392d4029ae986bfb8d4c
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-09-22 15:54:23 +0700

    Update request header on api .js

--- src/pages/api/api.js
+++ src/pages/api/api.js
@@ -23,7 +23,6 @@ export class Api {
   }
 
   getResponse(url, postbody, method) {
-    const api = new Api();
     return new Promise((resolve, reject) => {
       const resolver = function (response) {
         if (response && response.data) {
@@ -56,11 +55,11 @@ export class Api {
 
       switch (method) {
         case "get":
-          return api.client.get(url, postbody).then(resolver).catch(rejecter);
+          return this.client.get(url, postbody).then(resolver).catch(rejecter);
         case "post":
-          return api.client.post(url, postbody).then(resolver).catch(rejecter);
+          return this.client.post(url, postbody).then(resolver).catch(rejecter);
         default:
-          return api.client.get(url, postbody).then(resolver).catch(rejecter);
+          return this.client.get(url, postbody).then(resolver).catch(rejecter);
       }
     });
   }
