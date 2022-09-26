6fae28fbd2c7996af2a7666974c3f7752cf68263
surendra.patidar <suredra.patidar@photon.com>
2022-09-26 15:13:15 +0530

    Bugsfix-s4/EPP-5326,EPP-5327,EPP-5328

--- src/components/molecules/DayAvailability/DayAvailability.jsx
+++ src/components/molecules/DayAvailability/DayAvailability.jsx
@@ -189,7 +189,7 @@ export const DayAvailability = ({
           <Button
             role={"button"}
             onClick={() => {
-              if (!isPrevArrowDisable(dateList)) {
+              if (!isPrevArrowDisable(dateList, new Date())) {
                 const date = new Date(dateList.dateRange[0]);
                 date.setDate(date.getDate() - 7);
                 onPrevScheduleClicked("overlay", date);
@@ -205,7 +205,7 @@ export const DayAvailability = ({
           >
             <ArrowBackIosIcon
               className={
-                isPrevArrowDisable(dateList)
+                isPrevArrowDisable(dateList, new Date())
                   ? styles.iconDisableSchedule
                   : styles.iconSchedule
               }
