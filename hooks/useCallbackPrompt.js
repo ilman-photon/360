7843b7b76cdba422f7eaf2098ba8db4952734ff0
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-09-28 01:32:44 +0700

    Update component mock for router.replace

--- hooks/useCallbackPrompt.js
+++ hooks/useCallbackPrompt.js
@@ -1,17 +1,16 @@
 import { useEffect } from "react";
-import Router from "next/router";
 import { useBeforeUnload } from "react-use";
 
 export const useLeavePageConfirm = ({
   isConfirm = true,
-  message = "Are you sure want to leave this page?"
+  message = "Are you sure want to leave this page?",
 }) => {
   useBeforeUnload(isConfirm, message);
 
   // close
   const handleWindowClose = (e) => {
     e.preventDefault();
-    
+
     return (e.returnValue = "Change that you made might not be saved.");
   };
   useEffect(() => {
