29d6b74bcea863808cf4289acdd5209c9594e866
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-09-28 13:39:19 +0000

    fix(EPP-6132): cssfixing UI profile-info & insurance-info; fixing unit-test

--- src/components/atoms/RowRadioButtonsGroup/rowRadioButtonsGroup.jsx
+++ src/components/atoms/RowRadioButtonsGroup/rowRadioButtonsGroup.jsx
@@ -24,6 +24,7 @@ export default function RowRadioButtonsGroup({
   tooltipContent,
   textSx = {},
   isCancelSchedule = false,
+  isInsuranceForm = false,
   ...props
 }) {
   const options = props.options || [];
