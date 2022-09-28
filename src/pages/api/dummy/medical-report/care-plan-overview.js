ba752319a33d2d265d169261035b08eb9a08470b
satia rahman <satia.r@photon.com>
2022-09-28 19:22:15 +0700

    adjust dummy

--- src/pages/api/dummy/medical-report/care-plan-overview.js
+++ src/pages/api/dummy/medical-report/care-plan-overview.js
@@ -1,16 +1,17 @@
 export default function CarePlanOverview(req, res) {
-  const createData = (name, date) => {
+  const createData = (id, name, date) => {
     return {
+      id,
       name,
       date,
     };
   };
 
   const dummyDocuments = [
-    createData("Eye Surgery", "09/09/2022 12:00PM"),
-    createData("Eye Surgery", "09/09/2022 12:00PM"),
-    createData("Eye Surgery", "09/09/2022 12:00PM"),
-    createData("Eye Surgery", "08/08/2022 12:00PM"),
+    createData(1, "Eye Surgery", "09/09/2022 12:00PM"),
+    createData(2, "Eye Surgery", "09/09/2022 12:00PM"),
+    createData(3, "Eye Surgery", "09/09/2022 12:00PM"),
+    createData(4, "Eye Surgery", "08/08/2022 12:00PM"),
   ];
 
   if (req.method === "GET") {
