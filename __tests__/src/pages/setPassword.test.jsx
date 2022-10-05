8c34abe89acb82fd0e54514841f20e22074cd6cf
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-10-06 01:55:05 +0700

    Increase sonar coverage for set-password

--- __tests__/src/pages/setPassword.test.jsx
+++ __tests__/src/pages/setPassword.test.jsx
@@ -5,55 +5,96 @@ import store from "../../../src/store/store";
 import SetPasswordPage, {
   getServerSideProps,
 } from "../../../src/pages/patient/set-password";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import { sleep } from "../../../__mocks__/util";
 
 describe("Set Password", () => {
   let container;
-  beforeEach(async () => {
+  const mock = new MockAdapter(axios);
+  const renderSetPassword = async (email = "") => {
     const contex = {
-      query: {
-        username: "smith1@photon.com",
-      },
+      query: email
+        ? {
+            username: email,
+          }
+        : {},
     };
-    getServerSideProps(contex);
+    const serverProps = await getServerSideProps(contex);
     container = render(
       <Provider store={store}>
-        {SetPasswordPage.getLayout(<SetPasswordPage />)}
+        {SetPasswordPage.getLayout(<SetPasswordPage {...serverProps.props} />)}
       </Provider>
     );
-    await waitFor(() => {
-      container.getByText(/Set Password/i);
-    });
+    await waitFor(() => container.getByText(/Set Password/i));
+  };
+  beforeEach(async () => {
+    mock
+      .onPost("/ecp/patient/registrationsetpassword")
+      .reply(200, { responseCode: 1000 });
   });
 
-  test("check valid email", () => {
-    setTimeout(() => {
-      const title = container.getByText(/title/i);
-      expect("title").toEqual(title.textContent);
-    }, 500);
+  afterAll(() => {
+    jest.resetAllMocks();
   });
 
-  test("click reset password", async () => {
-    // setTimeout(() => {
-    await waitFor(() => {
-      container.getByText(/Set Password/i);
-    });
-    //const password = container.getById(/password/i);
+  test("check no valid email", async () => {
+    await renderSetPassword();
+    const title = container.getByText(/Set Password/i);
+    expect("Set Password").toEqual(title.textContent);
+  });
+
+  test("check valid phonenumber", async () => {
+    await renderSetPassword("1234567890");
+    const title = container.getByText(/Set Password/i);
+    expect("Set Password").toEqual(title.textContent);
+  });
+
+  test("check valid email", async () => {
+    await renderSetPassword("smith1@photon.com");
+    const title = container.getByText(/Set Password/i);
+    expect("Set Password").toEqual(title.textContent);
+  });
+
+  test("click reset password success", async () => {
+    await renderSetPassword("smith1@photon.com");
+    await waitFor(() => container.getByText(/Set Password/i));
     const password = container.container.querySelector("#password");
     fireEvent.change(password, { target: { value: "user123@A" } });
     expect(password.value).toEqual("user123@A");
-
-    // const confirmPassword = container.getById(
-    //   /confirmPassword/i
-    // );
     const confirmPassword =
       container.container.querySelector("#confirmPassword");
     fireEvent.change(confirmPassword, { target: { value: "user123@A" } });
     expect(confirmPassword.value).toEqual("user123@A");
-    // }, 500);
 
     const submitbutton = container.getByRole("button", {
       name: /Create Account/i,
     });
     fireEvent.click(submitbutton);
+    await sleep(4000);
+    await waitFor(() => container.getByText(/Back to Login/i));
+    fireEvent.click(container.getByText(/Back to Login/i));
+  });
+
+  test("click reset password failed", async () => {
+    mock
+      .onPost("/ecp/patient/registrationsetpassword")
+      .reply(401, { ResponseCode: 3002 });
+    await renderSetPassword("smith1@photon.com");
+    await waitFor(() => container.getByText(/Set Password/i));
+    const password = container.container.querySelector("#password");
+    fireEvent.change(password, { target: { value: "user123@A" } });
+    expect(password.value).toEqual("user123@A");
+    const confirmPassword =
+      container.container.querySelector("#confirmPassword");
+    fireEvent.change(confirmPassword, { target: { value: "user123@A" } });
+    expect(confirmPassword.value).toEqual("user123@A");
+
+    const submitbutton = container.getByRole("button", {
+      name: /Create Account/i,
+    });
+    fireEvent.click(submitbutton);
+    await waitFor(() => container.getByText(/Back to home/i));
+    expect(container.getByText(/Back to home/i)).toBeInTheDocument();
   });
 });
