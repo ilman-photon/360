4578e81851de10aa287fb2e7d56dcd68feaad2f6
Fajar Apriansyah <fajar.apriansyah@photon.com>
2022-10-18 11:28:46 +0700

    fix: add store import on login page

--- src/pages/patient/login/index.js
+++ src/pages/patient/login/index.js
@@ -6,6 +6,7 @@ import { useEffect } from "react";
 import constants from "../../../utils/constants";
 import { removeAuthCookies } from "../../../utils/authetication";
 import Provider from "../../../store/provider";
+import store from "../../../store";
 
 const api = new Api();
 const cookies = new Cookies();
