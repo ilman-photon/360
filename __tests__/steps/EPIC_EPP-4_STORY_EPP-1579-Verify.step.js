1d392048622586bb3895ce326764e53bd09912f7
Risyad Fadillah <risyad.f@photoninfotech.net>
2022-10-20 17:49:22 +0700

    Increase coverage for phone number, remove medicalReport and update EPP-1578, EPP-1579

--- __tests__/steps/EPIC_EPP-4_STORY_EPP-1579-Verify.step.js
+++ __tests__/steps/EPIC_EPP-4_STORY_EPP-1579-Verify.step.js
@@ -1,11 +1,13 @@
 import "@testing-library/jest-dom/extend-expect";
-import { act, fireEvent, render } from "@testing-library/react";
+import { fireEvent, cleanup, waitFor } from "@testing-library/react";
 import axios from "axios";
 import MockAdapter from "axios-mock-adapter";
 import { defineFeature, loadFeature } from "jest-cucumber";
-import ForgotPasswordPage from "../../src/pages/patient/forgot-password";
-import AuthPage from "../../src/pages/patient/login";
-import Cookies from "universal-cookie";
+import {
+  renderLogin,
+  renderForgotPassword,
+} from "../../__mocks__/commonSteps";
+import ForgotPassword from "../../src/components/organisms/ForgotPassword/forgotPassword";
 
 jest.mock("universal-cookie", () => {
   class MockCookies {
@@ -30,6 +32,7 @@ const feature = loadFeature(
 );
 
 defineFeature(feature, (test) => {
+  let container
   const mock = new MockAdapter(axios);
   test("EPIC_EPP-44_STORY_EPP-1579 - Verify user able to view inline error message if invalid Email or Phone number to Sync eye exam is  entered", ({
     given,
@@ -37,26 +40,46 @@ defineFeature(feature, (test) => {
     when,
     then,
   }) => {
-    given("user launch the Marketing Site url", () => {});
+    given("user launch the Marketing Site url", () => { });
 
-    and("user clicks on the Schedule your Eye Exam button", () => {});
+    and("user clicks on the Schedule your Eye Exam button", () => { });
 
-    and("user click on Continue as a Guest option", () => {});
+    and("user click on Continue as a Guest option", () => { });
 
-    when(
-      "user click on Already have an appointment? Sync your appointment information button",
-      () => {}
-    );
+    when("user click on Already have an appointment? Sync your appointment information button", async () => {
+      container = await renderLogin()
+      const syncButton = container.getByText("syncYourAppointmentInformation");
+      fireEvent.click(syncButton);
+    });
 
-    then("user should see the Email or Phone number", () => {});
+    then("user should see the Email or Phone number", async () => {
+      cleanup()
+      container = await renderForgotPassword()
+      expect(container.getByLabelText(/usernamePlaceHolder/i)).toBeInTheDocument()
+    });
 
-    and(/^user provides invalid (.*)$/, (arg0) => {});
+    and(/^user provides invalid (.*)$/, (arg0) => {
+      const userField = container.getByLabelText(/usernamePlaceHolder/i);
+      fireEvent.change(userField, { target: { value: "wrongUser" } });
+      expect(userField.value).toEqual("wrongUser");
+    });
 
-    and("user should click on submit", () => {});
+    and("user should click on submit", async () => {
+      container.rerender(
+        <ForgotPassword isAppointment={true} />
+      );
+      expect(
+        await waitFor(() =>
+          container.getByText(/syncButton/i)
+        )
+      ).toBeInTheDocument();
+      const syncButton = container.getByText(/syncButton/i);
+      fireEvent.click(syncButton);
+    });
 
     then(
       "user should see error message Invalid Email or Phone number",
-      () => {}
+      () => { }
     );
   });
 });
