2d5bd09d3b5be7e2ac80cbb723896d3939ca892c
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-10-23 12:16:27 +0700

    Fix add to calender unit test

--- __tests__/src/utils/addToCalender.test.jsx
+++ __tests__/src/utils/addToCalender.test.jsx
@@ -1,17 +1,43 @@
 import "@testing-library/jest-dom";
 import { addToCalendar } from "../../../src/utils/addToCalendar";
 
-const moment = require('moment-timezone');
-jest.doMock('moment', () => {
-  moment.tz.setDefault('America/New_York');
+// const moment = require('moment-timezone');
+// jest.doMock('moment', () => {
+//   moment.tz.setDefault('America/New_York');
+//   return moment;
+// });
+jest.mock("moment", () => {
+  const originalModule = jest.requireActual("moment");
+  const moment = {
+    __esModule: true,
+    ...originalModule,
+    default: jest.fn((date) => {
+      const objNew = new originalModule(date);
+      objNew.tz = jest.fn().mockReturnValue(objNew);
+      return {
+        ...objNew,
+      };
+    }),
+    tz: { setDefault: jest.fn() },
+  };
   return moment;
 });
 
-global.crypto = require('crypto')
-
 describe("addToCalender Util", () => {
   test("addToCalender funtionality", () => {
-    const data = { name: "Robert", description: "lorem ipsum", location: "jl. Raya bogor, Kabupaten Bogor", date: "2022-09-09" }
-    addToCalendar(data)
+    const crypto = require("crypto");
+
+    Object.defineProperty(global.self, "crypto", {
+      value: {
+        getRandomValues: (arr) => crypto.randomBytes(arr.length),
+      },
+    });
+    const data = {
+      name: "Robert",
+      description: "lorem ipsum",
+      location: "jl. Raya bogor, Kabupaten Bogor",
+      date: "2022-09-09",
+    };
+    addToCalendar(data);
   });
 });
