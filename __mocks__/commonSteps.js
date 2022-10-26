f0008e95c6d4447eddcae9bc7b7367fa62df20d6
Dewo Sidharta Simanjuntak <dewo.sidharta@photoninfotech.net>
2022-10-26 18:29:50 +0700

    Update common step to render dashboard screen

--- __mocks__/commonSteps.js
+++ __mocks__/commonSteps.js
@@ -17,7 +17,7 @@ import axios from "axios";
 import mediaQuery from "css-mediaquery";
 import FilterResult from "../src/components/molecules/FilterResult/filterResult";
 import ScheduleAppointment from "../src/pages/patient/schedule-appointment/index";
-import HomePage from "../src/pages/patient";
+import HomePage, { getStaticProps } from "../src/pages/patient";
 import Cookies from "universal-cookie";
 import App from "../src/pages/_app";
 import CreateAccountPage from "../src/pages/patient/auth/create-account";
@@ -27,296 +27,26 @@ import {
   TEMP_DATA_GLASSES,
   TEMP_DATA_MEDICATION,
 } from "./component-mock";
-
-const MOCK_APPOINTMENT = {
-  appointmentList: [
-    {
-      appointmentId: "1",
-      providerInfo: {
-        providerId: "1",
-        name: "Paul Wagner Md",
-        position: "Scripps Eyecare",
-        address: {
-          addressLine1: "51 West 51st Street",
-          addressLine2: "Floor 3, Suite 320 Midtown",
-          city: "Florida",
-          state: "FR",
-          zipcode: "54231",
-        },
-        rating: "5",
-        phoneNumber: "8572999989",
-        distance: "10 mi",
-        image: "/doctor.png",
-        from: "2022-07-18",
-        to: "2022-07-23",
-        location: {
-          latitude: 32.751204,
-          longitude: -117.1641166,
-        },
-      },
-      patientInfo: {
-        name: "Rebecca Chan",
-        firstname: "Rebecca",
-        lastname: "Chan",
-        dob: "12/12/2022",
-        phoneNumber: "1234567890",
-      },
-      appointmentInfo: {
-        appointmentType: "Eye Exam",
-        date: "Thu, 12 Jan 2023 04:30:00 EST",
-        insuranceCarrier: ["ECP Vision", "BlueCare Vision"],
-      },
-    },
-    {
-      appointmentId: "1",
-      providerInfo: {
-        providerId: "1",
-        name: "Dr. Sonha Nguyen",
-        position: "Scripps Eyecare",
-        address: {
-          addressLine1: "51 West 51st Street",
-          addressLine2: "Floor 3, Suite 320 Midtown",
-          city: "Florida",
-          state: "FR",
-          zipcode: "54231",
-        },
-        rating: "5",
-        phoneNumber: "8572999989",
-        distance: "10 mi",
-        image: "/doctor.png",
-        from: "2022-07-18",
-        to: "2022-07-23",
-        location: {
-          latitude: 32.751204,
-          longitude: -117.1641166,
-        },
-      },
-      patientInfo: {
-        name: "Rebecca Chan",
-        firstname: "Rebecca",
-        lastname: "Chan",
-        dob: "12/12/2022",
-        phoneNumber: "1234567890",
-      },
-      appointmentInfo: {
-        appointmentType: "Eye Exam",
-        date: "Thu, 12 Jan 2023 04:30:00 EST",
-        insuranceCarrier: ["ECP Vision", "BlueCare Vision"],
-      },
-    },
-  ],
-};
-
-const MOCK_PRESCRIPTION = {
-  prescriptions: {
-    glasses: [
-      {
-        prescribedBy: "Dr. Sonha Nguyen",
-        date: "2022-09-02T11:18:47.229Z",
-        expirationDate: "2022-10-02T11:18:47.229Z",
-        prescriptionDetails: [
-          {
-            Eye: "OD",
-            Sph: "+20.00",
-            Cyl: "-5.00",
-            Axis: "70",
-            Add: "x180",
-          },
-          {
-            Eye: "OS",
-            Sph: "+19.75",
-            Cyl: "-4.75",
-            Axis: "38",
-            Add: "x090",
-          },
-        ],
-      },
-    ],
-    contacts: [
-      {
-        prescribedBy: "Dr. Sonha Nguyen",
-        date: "2022-09-02T11:18:47.229Z",
-        expirationDate: "2022-10-02T11:18:47.229Z",
-        prescriptionDetails: [
-          {
-            Eye: "OD",
-            Sph: "+20.00",
-            Bc: "-5.00",
-            Cyl: "70",
-            Axis: "x180",
-          },
-          {
-            Eye: "OS",
-            Sph: "+19.75",
-            Bc: "-4.75",
-            Cyl: "38",
-            Axis: "x090",
-          },
-        ],
-      },
-    ],
-    medications: [
-      {
-        prescription: "Aspirint 0.1% Ointmanet",
-        date: "2022-09-02T11:18:47.229Z",
-      },
-      {
-        prescription: "Aspirint 0.1% Ointmanet",
-        date: "2022-09-02T11:18:47.229Z",
-      },
-    ],
-  },
-};
-
-const MOCK_SUGESTION = {
-  appointmentType: [
-    {
-      id: "1",
-      name: "Eye Exam",
-      description: "Test the health of your eye",
-    },
-    {
-      id: "2",
-      name: "Follow up",
-      description: "See your doctor today",
-    },
-    {
-      id: "3",
-      name: "Comprehensive",
-      description: "Get detailed eye exam",
-    },
-    {
-      id: "4",
-      name: "Contacts Only",
-      description: "Get fitted for the right contacts",
-    },
-  ],
-  insuranceCarrier: {
-    general: [
-      {
-        id: "1",
-        name: "I'm paying out of my pocket",
-      },
-      {
-        id: "2",
-        name: "skip and choose insurance later",
-      },
-      {
-        id: "3",
-        name: "Other Insurance",
-      },
-    ],
-    popular: [
-      {
-        id: "4",
-        name: "Aetna",
-      },
-      {
-        id: "5",
-        name: "Aetna",
-      },
-      {
-        id: "6",
-        name: "Blue Cross Blue Shield",
-      },
-      {
-        id: "7",
-        name: "Cigna",
-      },
-    ],
-    all: [
-      {
-        id: "8",
-        name: "Kaiser",
-      },
-    ],
-  },
-  filterbyData: [
-    {
-      name: "Available Today",
-      checked: false,
-    },
-    {
-      name: "language",
-      checklist: [
-        {
-          name: "Arabic",
-          checked: false,
-        },
-        {
-          name: "Chinese",
-          checked: false,
-        },
-        {
-          name: "English",
-          checked: false,
-        },
-        {
-          name: "Farsi",
-          checked: false,
-        },
-        {
-          name: "French",
-          checked: false,
-        },
-        {
-          name: "Spanish",
-          checked: false,
-        },
-        {
-          name: "Portuguese",
-          checked: false,
-        },
-        {
-          name: "Korean",
-          checked: false,
-        },
-        {
-          name: "German",
-          checked: false,
-        },
-        {
-          name: "Italian",
-          checked: false,
-        },
-        {
-          name: "Indonesian",
-          checked: false,
-        },
-      ],
-    },
-    {
-      name: "Insurance",
-      checklist: [
-        {
-          name: "In Network",
-          checked: false,
-        },
-        {
-          name: "Out of Network",
-          checked: false,
-        },
-      ],
-    },
-    {
-      name: "Gender",
-      checklist: [
-        {
-          name: "Male",
-          checked: false,
-        },
-        {
-          name: "Female",
-          checked: false,
-        },
-        {
-          name: "Non-Binary",
-          checked: false,
-        },
-      ],
-    },
-  ],
-};
+import {
+  mockAppointmentTypes,
+  prescriptionContact,
+  prescriptionGlasses,
+  prescriptionMedication,
+  submitFilter,
+  upcomingResponse,
+} from "./mockResponse";
+jest.mock("universal-cookie", () => {
+  class MockCookies {
+    static result = {};
+    get() {
+      return MockCookies.result;
+    }
+    remove() {
+      return jest.fn();
+    }
+  }
+  return MockCookies;
+});
 
 const providerList = [
   {
@@ -749,52 +479,45 @@ export async function renderAppointmentDetail() {
 }
 
 export async function navigateToPatientPortalHome() {
-  let container;
-  const element = document.createElement("div");
   const mock = new MockAdapter(axios);
-  Cookies.result = "true";
-  const expectedResult = {
-    ResponseCode: 2005,
-    ResponseType: "success",
-  };
-  const domain = window.location.origin;
-  mock.onPost(`/ecp/patient/logout`).reply(200, expectedResult);
-  mock
-    .onGet(`${domain}/api/dummy/appointment/create-appointment/getSugestion`)
-    .reply(200, MOCK_SUGESTION);
-  mock
-    .onGet(`${domain}/api/dummy/appointment/my-appointment/getAllAppointment`)
-    .reply(200, MOCK_APPOINTMENT);
-  mock
-    .onGet(`${domain}/api/dummy/appointment/my-appointment/getAllPrescriptions`)
-    .reply(200, MOCK_PRESCRIPTION);
-  mock
-    .onGet(`/ecp/prescriptions/patient/98f9404b-6ea8-4732-b14f-9c1a168d8066`)
-    .reply(200, TEMP_DATA_MEDICATION);
-  mock
-    .onGet(
-      `/ecp/prescriptions/patient/98f9404b-6ea8-4732-b14f-9c1a168d8066/getContactsData`
-    )
-    .reply(200, TEMP_DATA_CONTACTS);
-  mock
-    .onGet(
-      `/ecp/prescriptions/patient/98f9404b-6ea8-4732-b14f-9c1a168d8066/getGlassesData`
-    )
-    .reply(200, TEMP_DATA_GLASSES);
+  mock.reset();
   const mockGeolocation = {
     getCurrentPosition: jest.fn(),
     watchPosition: jest.fn(),
   };
   global.navigator.geolocation = mockGeolocation;
-  Cookies.result = false;
+  Cookies.result = "true";
+  mock
+    .onPut("/ecp/appointments/available-slot?searchText=Texas")
+    .reply(200, submitFilter);
+  mock
+    .onGet(`/ecp/appointments/appointment-types`)
+    .reply(200, mockAppointmentTypes);
+  const userData = JSON.parse(localStorage.getItem("userData"));
+  mock
+    .onGet(new RegExp(`/ecp/appointments/${userData?.patientId}/upcoming`))
+    .reply(200, upcomingResponse);
+  mock
+    .onGet(`/ecp/prescriptions/patient/${userData?.patientId}`)
+    .reply(200, prescriptionMedication);
+  mock
+    .onGet(`/ecp/prescriptions/patient/${userData?.patientId}/getGlassesData`)
+    .reply(200, prescriptionGlasses);
+  mock
+    .onGet(`/ecp/prescriptions/patient/${userData?.patientId}/getContactsData`)
+    .reply(200, prescriptionContact);
+
+  let container;
+  const props = await getStaticProps();
   act(() => {
     container = render(
-      <Provider store={store}>{HomePage.getLayout(<HomePage />)}</Provider>
+      <Provider store={store}>
+        {HomePage.getLayout(<HomePage {...props} />)}
+      </Provider>
     );
   });
-  await waitFor(() =>
-    container.getByText(constants.TEST_ID.HOME_TEST_ID.logout)
-  );
+  await waitFor(() => container.getAllByText(/Welcome to your dashboard/i));
+  return { ...container, mock };
 }
 
 export async function landOnCreateAccountPage() {
