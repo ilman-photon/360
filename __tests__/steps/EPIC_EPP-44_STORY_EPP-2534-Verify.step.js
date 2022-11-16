7c83d5bd73db52be04b5f37e1f1402a93c74c5ee
Risyad Fadillah <risyad.f@photoninfotech.net>
2022-11-16 17:45:58 +0700

    update BDD unit test EPP-251 and EPP-2534

--- __tests__/steps/EPIC_EPP-44_STORY_EPP-2534-Verify.step.js
+++ __tests__/steps/EPIC_EPP-44_STORY_EPP-2534-Verify.step.js
@@ -1,24 +1,24 @@
-import {
-  act,
-  fireEvent,
-  render,
-  waitFor,
-  cleanup,
-} from "@testing-library/react";
-import axios from "axios";
+import { fireEvent, render, waitFor, cleanup, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
-import MockAdapter from "axios-mock-adapter";
 import { defineFeature, loadFeature } from "jest-cucumber";
-import { Provider } from "react-redux";
-import store from "../../src/store/store";
-const useRouter = jest.spyOn(require("next/router"), "useRouter");
-import constants from "../../src/utils/constants";
+import MockAdapter from "axios-mock-adapter";
+import axios from "axios";
+import {
+  clickSearch,
+  createMatchMedia,
+  doLogin,
+  provideFilters,
+  renderLogin,
+  renderScheduleAppointment,
+} from "../../__mocks__/commonSteps";
+import constants, { TEST_ID } from "../../src/utils/constants";
+import {
+  mockAppointmentTypes,
+  mockInsurance,
+  submitFilter,
+} from "../../__mocks__/mockResponse";
 import AuthPage from "../../src/pages/patient/login";
-import Cookies from "universal-cookie";
-import { getServerSideProps } from "../../src/pages/patient/mfa";
-import HomePage from "../../src/pages/patient";
-import { renderScheduleAppointment } from "../../__mocks__/commonSteps";
-import Appointment from "../../src/pages/patient/appointment";
+import { renderWithProviders } from "../src/utils/test-util";
 
 const feature = loadFeature(
   "./__tests__/feature/Patient Portal/Sprint4/EPP-2534.feature"
@@ -28,26 +28,64 @@ const feature = loadFeature(
 defineFeature(feature, (test) => {
   let container;
   const element = document.createElement("div");
+  const defaultValidation = () => {
+    expect(true).toBeTruthy();
+  };
   const mock = new MockAdapter(axios);
+  const { FORGOT_TEST_ID, APPOINTMENT_TEST_ID } = TEST_ID;
+  afterEach(() => {
+    cleanup()
+    mock.reset();
+  });
+
+  beforeEach(() => {
+    const mockGeolocation = {
+      getCurrentPosition: jest.fn(),
+      watchPosition: jest.fn(),
+    };
+
+    mock
+      .onGet("/ecp/appointments/appointment-types", mockAppointmentTypes)
+      .reply(200, mockAppointmentTypes);
+    mock
+      .onGet("/ecp/appointments/insurance/allpayers", mockInsurance)
+      .reply(200, mockInsurance);
+    mock
+      .onPut("/ecp/appointments/available-slot?searchText=Texas")
+      .reply(200, submitFilter);
+    window.matchMedia = createMatchMedia("1920px");
+    global.navigator.geolocation = mockGeolocation;
+  });
+
   test('EPIC_EPP-44_STORY_EPP-2534 - Verify user able to search for location and select the date of appointment as well as purpose of visit and insurance.', ({ given, when, and, then }) => {
-    given('user launch the Patient Portal url', () => {
-      expect(true).toBeTruthy();
+    given('user launch the Patient Portal url', async () => {
+      container = await renderLogin(container);
     });
 
     when('user provides valid Email or Phone Number and password', () => {
-      expect(true).toBeTruthy();
+      act(() => {
+        container = renderWithProviders(<AuthPage />, {
+          container: document.body.appendChild(element),
+          legacyRoot: true,
+        });
+      });
+      const title = container.getByText("formTitle");
+      expect("formTitle").toEqual(title.textContent);
     });
 
     and('user clicks on Login button', () => {
-      expect(true).toBeTruthy();
+      const loginBtn = container.getByTestId(constants.TEST_ID.LOGIN_TEST_ID.loginBtn);
+      fireEvent.click(loginBtn)
+      expect(loginBtn).toBeInTheDocument();
     });
 
     then('user navigates to the Patient Portal application', () => {
       expect(true).toBeTruthy();
     });
 
-    when('user  clicks on Schedule Appointment menu', () => {
-      expect(true).toBeTruthy();
+    when('user  clicks on Schedule Appointment menu', async () => {
+      cleanup();
+      container = await renderScheduleAppointment(mock);
     });
 
     then('user navigates to the search screen', () => {
@@ -55,23 +93,29 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the location', () => {
-      expect(true).toBeTruthy();
+      const locationField = container.getByText("City, state, or zip code");
+      expect(locationField).toBeInTheDocument();
     });
 
     and('user selects the date of appointment', () => {
-      expect(true).toBeTruthy();
+      const dateField = container.getByText(/Date/i);
+      expect(dateField).toBeInTheDocument();
     });
 
     and('user chooses the purpose of the visit', () => {
-      expect(true).toBeTruthy();
+      const pusposeField = container.getByText(/Purpose of Visit/i);
+      expect(pusposeField).toBeInTheDocument();
     });
 
     and('user enters the insurance name', () => {
-      expect(true).toBeTruthy();
+      const pusposeField = container.getByText(/Insurance Carrier/i);
+      expect(pusposeField).toBeInTheDocument();
     });
 
     and('user clicks on the Search button', () => {
-      expect(true).toBeTruthy();
+      const searchbtn = container.getByTestId("searchbtn");
+      expect(searchbtn).toBeInTheDocument();
+      fireEvent.click(searchbtn)
     });
 
     and('user views the results on the Schedule Appointments screen', () => {
@@ -84,24 +128,34 @@ defineFeature(feature, (test) => {
   });
 
   test('EPIC_EPP-44_STORY_EPP-2534 - Verify user able to search for location and select the date of appointment as well as purpose of visit and insurance and user view the location', ({ given, when, and, then }) => {
-    given('user launch the Patient Portal url', () => {
-      expect(true).toBeTruthy();
+    given('user launch the Patient Portal url', async () => {
+      container = await renderLogin(container);
     });
 
     when('user provides valid Email or Phone Number and password', () => {
-      expect(true).toBeTruthy();
+      act(() => {
+        container = renderWithProviders(<AuthPage />, {
+          container: document.body.appendChild(element),
+          legacyRoot: true,
+        });
+      });
+      const title = container.getByText("formTitle");
+      expect("formTitle").toEqual(title.textContent);
     });
 
     and('user clicks on Login button', () => {
-      expect(true).toBeTruthy();
+      const loginBtn = container.getByTestId(constants.TEST_ID.LOGIN_TEST_ID.loginBtn);
+      fireEvent.click(loginBtn)
+      expect(loginBtn).toBeInTheDocument();
     });
 
     then('user navigates to the Patient Portal application', () => {
       expect(true).toBeTruthy();
     });
 
-    when('user  clicks on Schedule Appointment menu', () => {
-      expect(true).toBeTruthy();
+    when('user  clicks on Schedule Appointment menu', async () => {
+      cleanup();
+      container = await renderScheduleAppointment(mock);
     });
 
     then('user navigates to the search screen', () => {
@@ -109,11 +163,13 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the location', () => {
-      expect(true).toBeTruthy();
+      const locationField = container.getByText("City, state, or zip code");
+      expect(locationField).toBeInTheDocument();
     });
 
     and('user selects the date of appointment', () => {
-      expect(true).toBeTruthy();
+      const dateField = container.getByText(/Date/i);
+      expect(dateField).toBeInTheDocument();
     });
 
     and('user chooses the purpose of the visit', () => {
@@ -121,11 +177,14 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the insurance name', () => {
-      expect(true).toBeTruthy();
+      const pusposeField = container.getByText(/Insurance Carrier/i);
+      expect(pusposeField).toBeInTheDocument();
     });
 
     and('user clicks on the Search button', () => {
-      expect(true).toBeTruthy();
+      const searchbtn = container.getByTestId("searchbtn");
+      expect(searchbtn).toBeInTheDocument();
+      fireEvent.click(searchbtn)
     });
 
     and('user views the results in the Schedule Appointments screen', () => {
@@ -138,24 +197,34 @@ defineFeature(feature, (test) => {
   });
 
   test('EPIC_EPP-44_STORY_EPP-2534 - Verify the user is able to search for location and select the date of appointment as well as the purpose of visit and insurance and the user view the date of appointment', ({ given, when, and, then }) => {
-    given('user launch the Patient Portal url', () => {
-      expect(true).toBeTruthy();
+    given('user launch the Patient Portal url', async () => {
+      container = await renderLogin(container);
     });
 
     when('user provides valid Email or Phone Number and password', () => {
-      expect(true).toBeTruthy();
+      act(() => {
+        container = renderWithProviders(<AuthPage />, {
+          container: document.body.appendChild(element),
+          legacyRoot: true,
+        });
+      });
+      const title = container.getByText("formTitle");
+      expect("formTitle").toEqual(title.textContent);
     });
 
     and('user clicks on Login button', () => {
-      expect(true).toBeTruthy();
+      const loginBtn = container.getByTestId(constants.TEST_ID.LOGIN_TEST_ID.loginBtn);
+      fireEvent.click(loginBtn)
+      expect(loginBtn).toBeInTheDocument();
     });
 
     then('user navigates to the Patient Portal application', () => {
       expect(true).toBeTruthy();
     });
 
-    when('user  clicks on Schedule Appointment menu', () => {
-      expect(true).toBeTruthy();
+    when('user  clicks on Schedule Appointment menu', async () => {
+      cleanup();
+      container = await renderScheduleAppointment(mock);
     });
 
     then('user navigates to the search screen', () => {
@@ -163,11 +232,13 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the location', () => {
-      expect(true).toBeTruthy();
+      const locationField = container.getByText("City, state, or zip code");
+      expect(locationField).toBeInTheDocument();
     });
 
     and('user selects the date of appointment', () => {
-      expect(true).toBeTruthy();
+      const dateField = container.getByText(/Date/i);
+      expect(dateField).toBeInTheDocument();
     });
 
     and('user chooses the purpose of the visit', () => {
@@ -175,11 +246,14 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the insurance name', () => {
-      expect(true).toBeTruthy();
+      const pusposeField = container.getByText(/Insurance Carrier/i);
+      expect(pusposeField).toBeInTheDocument();
     });
 
     and('user clicks on the Search button', () => {
-      expect(true).toBeTruthy();
+      const searchbtn = container.getByTestId("searchbtn");
+      expect(searchbtn).toBeInTheDocument();
+      fireEvent.click(searchbtn)
     });
 
     and('user views the results in the Schedule Appointments screen', () => {
@@ -192,24 +266,34 @@ defineFeature(feature, (test) => {
   });
 
   test('EPIC_EPP-44_STORY_EPP-2534 - Verify the user is able to search for location and select the date of appointment as well as the purpose of visit and insurance and the user views the purpose of visit', ({ given, when, and, then }) => {
-    given('user launch the Patient Portal url', () => {
-      expect(true).toBeTruthy();
+    given('user launch the Patient Portal url', async () => {
+      container = await renderLogin(container);
     });
 
     when('user provides valid Email or Phone Number and password', () => {
-      expect(true).toBeTruthy();
+      act(() => {
+        container = renderWithProviders(<AuthPage />, {
+          container: document.body.appendChild(element),
+          legacyRoot: true,
+        });
+      });
+      const title = container.getByText("formTitle");
+      expect("formTitle").toEqual(title.textContent);
     });
 
     and('user clicks on Login button', () => {
-      expect(true).toBeTruthy();
+      const loginBtn = container.getByTestId(constants.TEST_ID.LOGIN_TEST_ID.loginBtn);
+      fireEvent.click(loginBtn)
+      expect(loginBtn).toBeInTheDocument();
     });
 
     then('user navigates to the Patient Portal application', () => {
       expect(true).toBeTruthy();
     });
 
-    when('user  clicks on Schedule Appointment menu', () => {
-      expect(true).toBeTruthy();
+    when('user  clicks on Schedule Appointment menu', async () => {
+      cleanup();
+      container = await renderScheduleAppointment(mock);
     });
 
     then('user navigates to the search screen', () => {
@@ -217,11 +301,13 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the location', () => {
-      expect(true).toBeTruthy();
+      const locationField = container.getByText("City, state, or zip code");
+      expect(locationField).toBeInTheDocument();
     });
 
     and('user selects the date of appointment', () => {
-      expect(true).toBeTruthy();
+      const dateField = container.getByText(/Date/i);
+      expect(dateField).toBeInTheDocument();
     });
 
     and('user chooses the purpose of the visit', () => {
@@ -229,11 +315,14 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the insurance name', () => {
-      expect(true).toBeTruthy();
+      const pusposeField = container.getByText(/Insurance Carrier/i);
+      expect(pusposeField).toBeInTheDocument();
     });
 
     and('user clicks on the Search button', () => {
-      expect(true).toBeTruthy();
+      const searchbtn = container.getByTestId("searchbtn");
+      expect(searchbtn).toBeInTheDocument();
+      fireEvent.click(searchbtn)
     });
 
     and('user views the results in the Schedule Appointments screen', () => {
@@ -246,24 +335,34 @@ defineFeature(feature, (test) => {
   });
 
   test('EPIC_EPP-44_STORY_EPP-2534 - Verify the user is able to search for location and select the date of appointment as well as the purpose of visit and insurance and the user view the insurance carrier.', ({ given, when, and, then }) => {
-    given('user launch the Patient Portal url', () => {
-      expect(true).toBeTruthy();
+    given('user launch the Patient Portal url', async () => {
+      container = await renderLogin(container);
     });
 
     when('user provides valid Email or Phone Number and password', () => {
-      expect(true).toBeTruthy();
+      act(() => {
+        container = renderWithProviders(<AuthPage />, {
+          container: document.body.appendChild(element),
+          legacyRoot: true,
+        });
+      });
+      const title = container.getByText("formTitle");
+      expect("formTitle").toEqual(title.textContent);
     });
 
     and('user clicks on Login button', () => {
-      expect(true).toBeTruthy();
+      const loginBtn = container.getByTestId(constants.TEST_ID.LOGIN_TEST_ID.loginBtn);
+      fireEvent.click(loginBtn)
+      expect(loginBtn).toBeInTheDocument();
     });
 
     then('user navigates to the Patient Portal application', () => {
       expect(true).toBeTruthy();
     });
 
-    when('user  clicks on Schedule Appointment menu', () => {
-      expect(true).toBeTruthy();
+    when('user  clicks on Schedule Appointment menu', async () => {
+      cleanup();
+      container = await renderScheduleAppointment(mock);
     });
 
     then('user navigates to the search screen', () => {
@@ -271,11 +370,13 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the location', () => {
-      expect(true).toBeTruthy();
+      const locationField = container.getByText("City, state, or zip code");
+      expect(locationField).toBeInTheDocument();
     });
 
     and('user selects the date of appointment', () => {
-      expect(true).toBeTruthy();
+      const dateField = container.getByText(/Date/i);
+      expect(dateField).toBeInTheDocument();
     });
 
     and('user chooses the purpose of the visit', () => {
@@ -283,11 +384,14 @@ defineFeature(feature, (test) => {
     });
 
     and('user enters the insurance name', () => {
-      expect(true).toBeTruthy();
+      const pusposeField = container.getByText(/Insurance Carrier/i);
+      expect(pusposeField).toBeInTheDocument();
     });
 
     and('user clicks on the Search button', () => {
-      expect(true).toBeTruthy();
+      const searchbtn = container.getByTestId("searchbtn");
+      expect(searchbtn).toBeInTheDocument();
+      fireEvent.click(searchbtn)
     });
 
     and('user views the results in the Schedule Appointments screen', () => {
