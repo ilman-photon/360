a6061f93e95e9c0bffff54846debcf087b49132e
Pratama Indra Raswoko <pratamaindra.r@photon.com>
2022-11-29 03:45:22 +0000

    update token expired and hide mfa code

--- src/pages/patient/mfa.js
+++ src/pages/patient/mfa.js
@@ -24,7 +24,6 @@ export default function MfaPage() {
   const [componentName, setComponentName] = React.useState("");
   const [rememberMe, setRememberMe] = React.useState(false);
   const [successSubmit, setSuccessSubmit] = React.useState(false);
-  const [otpValidation, setOTPValidation] = React.useState("");
   const [securityQuestionList, setSecurityQuestionList] = React.useState([]);
   const [communicationMethod, setCommunicationMethod] = React.useState({});
   const [isLoading, setIsLoading] = React.useState(true);
@@ -71,12 +70,6 @@ export default function MfaPage() {
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  const setTempValidation = (response) => {
-    if (process.env.ENV_NAME !== "prod" && response && response.mfaCode) {
-      setOTPValidation(response.mfaCode);
-    }
-  };
-
   function onConfirmClicked(communication, callback) {
     const deviceId = "";
     const postBody = {
@@ -87,7 +80,6 @@ export default function MfaPage() {
     api
       .sendMfaCode(postBody)
       .then((response) => {
-        setTempValidation(response);
         setComponentName(constants.MFA_COMPONENT_NAME);
         cookies.set("isStay", "stay", { path: "/patient" });
         cookies.set("mfaPreferredMode", communication, { path: "/patient" });
@@ -183,7 +175,6 @@ export default function MfaPage() {
     api
       .sendMfaCode(postBody)
       .then((response) => {
-        setTempValidation(response);
         callback({
           status: "success",
         });
@@ -278,21 +269,6 @@ export default function MfaPage() {
           setRememberMe={onSetRememberMe}
           testIds={MFA_TEST_ID}
         />
-        {otpValidation ? (
-          <Typography
-            aria-hidden={"true"}
-            style={{
-              opacity: 0.3,
-              position: "fixed",
-              bottom: 0,
-              left: 0,
-              fontSize: "12px",
-            }}
-            data-testid={"loc_validationMFA"}
-          >
-            {otpValidation}
-          </Typography>
-        ) : null}
       </>
     );
   } else if (componentName === constants.SQ_COMPONENT_NAME && isReady) {
