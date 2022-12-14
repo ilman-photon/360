314964d84a420f425d9cf969a5e6dd3ee8b473f0
Mohamad Nur Arifin <mohamadnur.a@photon.in>
2022-12-14 14:24:25 +0700

    Bugfix: EPP-11729

--- src/components/molecules/LoadingModal/LoadingModal.jsx
+++ src/components/molecules/LoadingModal/LoadingModal.jsx
@@ -1,15 +1,19 @@
 import { Box, Modal, CircularProgress } from "@mui/material";
 import React from "react";
-import { styles } from "../../organisms/BaseHeader/style";
 
-export const LoadingModal = ({open = false}) => {
+export const LoadingModal = ({ open = false }) => {
   return (
-    <Modal open={open} sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}>
-      <Box sx={{
-          "&:focus-visible" : {
-            outline:'none'
-          }
-      }} >       
+    <Modal
+      open={open}
+      sx={{ display: "flex", alignItems: "center", justifyContent: "center" }}
+    >
+      <Box
+        sx={{
+          "&:focus-visible": {
+            outline: "none",
+          },
+        }}
+      >
         <CircularProgress />
       </Box>
     </Modal>
