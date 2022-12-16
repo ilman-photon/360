c1f9d2d502c1f65bc941d5d46bd2d0b3d4d4449c
Dewo Sidharta Simanjuntak <dewo.sidharta@photon.com>
2022-12-16 14:14:34 +0000

    Poc/ga 4

--- src/pages/_document.js
+++ src/pages/_document.js
@@ -1,3 +1,4 @@
+/* eslint-disable @next/next/next-script-for-ga */
 import Document, { Html, Head, Main, NextScript } from "next/document";
 
 class CustomDocuments extends Document {
@@ -21,6 +22,21 @@ class CustomDocuments extends Document {
     return (
       <Html>
         <Head>
+          <script
+            id="google-analytics"
+            dangerouslySetInnerHTML={{
+              __html: `(function (w, d, s, l, i) {
+    w[l] = w[l] || [];
+    w[l].push({ "gtm.start": new Date().getTime(), event: "gtm.js" });
+    var f = d.getElementsByTagName(s)[0],
+      j = d.createElement(s),
+      dl = l != "dataLayer" ? "&l=" + l : "";
+    j.async = true;
+    j.src = "https://www.googletagmanager.com/gtm.js?id=" + i + dl;
+    f.parentNode.insertBefore(j, f);
+  })(window, document, "script", "dataLayer", "GTM-PNNWD4Q");`,
+            }}
+          />
           <link
             href="https://fonts.cdnfonts.com/css/bw-nista-geometric-demo"
             rel="stylesheet"
@@ -37,6 +53,14 @@ class CustomDocuments extends Document {
           ></link>
         </Head>
         <body>
+          <noscript>
+            <iframe
+              src="https://www.googletagmanager.com/ns.html?id=GTM-PNNWD4Q"
+              height="0"
+              width="0"
+              style={{ display: "none", visibility: "hidden" }}
+            ></iframe>
+          </noscript>
           <Main />
           <NextScript />
         </body>
